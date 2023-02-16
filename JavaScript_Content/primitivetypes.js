417979355bf720aa72039365283cba0f974a398c
Samuel-Gouvea <samuel.r.gouvea@gmail.com>
2023-02-15 21:52:19 -0300

    Organizing some primitive data types...

---
+++ JavaScript_Content/primitivetypes.js
@@ -0,0 +1,20 @@
+// Numbers:
+let length = 16;
+let weight = 84.2;
+
+// Strings:
+let color = "Yellow";
+let lastName = "Oilveira";
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object:
+const person = {firstName:"Samuel", lastName:"Gouvea"};
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-25");
