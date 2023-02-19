b4d0ba9f6a650e8353791cc3ff8757c6e7549806
Samuel-Gouvea <samuel.r.gouvea@gmail.com>
2023-02-19 13:30:19 -0300

    Updating...

--- JavaScript_Content/primitivetypes.js
+++ JavaScript_Content/primitivetypes.js
@@ -4,7 +4,7 @@ let weight = 84.2;
 
 // Strings:
 let color = "Yellow";
-let lastName = "Oilveira";
+let lastName = "Oliveira";
 
 // Booleans
 let x = true;
@@ -18,3 +18,9 @@ const cars = ["Saab", "Volvo", "BMW"];
 
 // Date object:
 const date = new Date("2022-03-25");
+
+// Four Ways to Declare a JavaScript Variable:
+
+let hombre = "Samuel Gouvea",
+carName = "Polo",
+price = 85.000;
