0387b52bc85e9c04a48e3ecbc442d8154eafcc23
Samuel-Gouvea <samuel.r.gouvea@gmail.com>
2023-02-26 21:37:24 -0300

    Updating...

--- JavaScript_Content/arraysort.js
+++ JavaScript_Content/arraysort.js
@@ -1,3 +1,13 @@
 const points = [40, 100, 1, 5, 25, 10];
 points.sort(function(a, b){return a - b});
 
+// array sort
+
+const pointsQuarter = [40, 100, 1, 5, 25, 10];
+
+for (let i = points.length -1; i > 0; i--) {
+  let j = Math.floor(Math.random() * (i+1));
+  let k = points[i];
+  points[i] = points[j];
+  points[j] = k;
+}
