27b40c5665cc5f3588f3124d3d8afb45fc7f4641
Samuel-Gouvea <samuel.r.gouvea@gmail.com>
2023-03-02 22:09:48 -0300

    Setting map values

--- JavaScript_Content/map.js
+++ JavaScript_Content/map.js
@@ -4,3 +4,11 @@ const fruits = new Map([
     ["bananas", 300],
     ["oranges", 200]
   ]);
+
+  // Create a Another Map
+const fruitss = new Map();
+
+// Set Map Values
+fruitss.set("apples", 500);
+fruitss.set("bananas", 300);
+fruitss.set("oranges", 200);
